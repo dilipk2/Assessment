21238bff1a9afd4250535670ee094317ca8fbcdc
dilipk2 <50397817+dilipk2@users.noreply.github.com>
2019-05-08 13:58:52 +0530

    Create Asses1.c

---
+++ Asses1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+int fact(int );
+{
+int n,fib;
+printf("enter a number to generate factorial")
+scanf("%d",n)
+f=fact(n);
+print("%d",f)
+}
+int fact(int a)
+{
+if(a==0)
+{
+return 1;
+}
+else
+{
+return fact*fact(a-1)
+}
+}
